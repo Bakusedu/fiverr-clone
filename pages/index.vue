566c1465e8324c808778d7c57ba7c356d57e5fcd
Chiedu Emmanuel <iamnedu912@gmail.com>
2021-02-28 13:08:39 +0100

    home animation smoothing

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@ export default {}
 </script>
 <style scoped>
   .hero-background {
-    animation: fadeInFadeOut 20s infinite;
+    animation: fadeInFadeOut 60s infinite;
     animation-timing-function: ease-in-out;
     background-size: cover;
     background-position: center;
@@ -24,32 +24,37 @@ export default {}
     width:100%;
     height:100vh;
     opacity: 0;
-    transition: opacity .9s
+    transition-property:opacity;
+    transition-duration:.2s;
   }
   @keyframes fadeInFadeOut {
     0% {
       background-image: url('https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049984/bg-hero-1-1792-x2.png');
       opacity:1;
     }
-    25% {
+    20% {
       background-image: url('https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/2413b8415dda9dbd7756d02cb87cd4b1-1599595203021/bg-hero-2-1792-x2.png');
       opacity:1;
     }
-    50% {
+    40% {
       background-image: url('https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/d14871e2d118f46db2c18ad882619ea8-1599835783974/bg-hero-3-1792-x2.png');
       opacity:1;
     }
-    75% {
+    60% {
       background-image: url('https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/93085acc959671e9e9e77f3ca8147f82-1599427734106/bg-hero-4-1792-x2.png');
       opacity:1;
     }
-    100% {
+    80% {
       background-image: url('https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049977/bg-hero-5-1792-x2.png');
+      opacity:1;
+    }
+    100% {
+      background-image: url('https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049984/bg-hero-1-1792-x2.png');
       opacity:1;
     }
   }
 
-  .hero-background:before {
+  /* .hero-background:before {
     content:"";
     position:absolute;
     top:0;
@@ -58,5 +63,5 @@ export default {}
     right:0;
     background-color:black;
     opacity:0.3;
-  }
+  } */
 </style>
